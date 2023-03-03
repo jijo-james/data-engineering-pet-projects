57f0f05a8e56f3ac766047af705ecdaeb4152d3b
Jijo James <jj.jamesjijo@gmail.com>
2023-03-03 14:39:04 +0530

    Load is completed Successfully

--- spotify-data/main.py
+++ spotify-data/main.py
@@ -6,11 +6,11 @@ from datetime import datetime
 import datetime
 import sqlite3
 
-DATABASE_LOCATION = 'sqlite://my_played_tracks.sqlite'
+DATABASE_LOCATION = 'sqlite:///my_played_tracks.sqlite'
 USER_ID = '31bkuggdupa7tuulnzslhhd4xmpe'
 TOKEN = 'BQAaH7lsRgdX-4Vkj9ddM5jVlrYAZdNZYbWuz6wySoe3qTmMHohqbzSsNYC3N1Q5RJr8KIgVzEfheixzQpaaYAz8Q4Ln5BVe_xmgRYAf96SR03caEZPkZ9eBzg8fYlnDc9k7Kb4t2tXvEh--3xWZy9V3aJOu2OuacmESn7ivztvH9Qs5TaCf6iH1CZfZKVBkI5vrKklTwGZadg'
 
-
+#Transform
 def check_if_valid_data(df: pd.DataFrame) -> bool:
 
     if df.empty:
@@ -71,3 +71,30 @@ if __name__ == '__main__':
     if check_if_valid_data(song_df):
         print("Data is valid")
         print(song_df.shape[0])
+    
+    #Load
+
+    engine = sqlalchemy.create_engine(DATABASE_LOCATION)
+    connection = sqlite3.connect('my_played_tracks.sqlite')
+    cursor = connection.cursor()
+
+    sqlquery = """
+    CREATE TABLE IF NOT EXISTS my_played_tracks(
+        song_name VARCHAR(200),
+        artist_name VARCHAR(200),
+        played_at VARCHAR(200),
+        timestamp VARCHAR(200),
+        CONSTRAINT primary_key_constraint PRIMARY KEY (played_at)
+    )
+    """
+
+    cursor.execute(sqlquery)
+    print("Opend DB sucessfully")
+
+    try:
+        song_df.to_sql("my_played_tracks", engine, index=False, if_exists='append')
+    except:
+        print("Data already exists in the DB")
+    
+    connection.close()
+    print("Close DB sucessfully")
